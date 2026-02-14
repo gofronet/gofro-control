fe5cd051e28e7a268a1ecd906348b0e5c1f89ac3
Georgiy Gusev <georgiygusev@Georgiys-MacBook-Air.local>
2026-02-14 17:44:42 +0300

    refactor delivery

---
+++ delivery/node_manage_handlers.go
@@ -0,0 +1,44 @@
+package delivery
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (router *Router) GetAllNodesHandler(w http.ResponseWriter, r *http.Request) {
+	nodesInfo, err := router.nodeManager.ListNodesInfo()
+	if err != nil {
+		RespondErr(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	resps := make([]NodeInfoResponse, 0, len(nodesInfo))
+	for _, nodeInfo := range nodesInfo {
+		resps = append(resps, NodeInfoResponse{
+			NodeName:    nodeInfo.NodeName,
+			XrayRunning: nodeInfo.IsXrayRunning,
+		})
+	}
+
+	RespondSuccess(w, http.StatusOK, resps)
+}
+
+func (router *Router) AddNode(w http.ResponseWriter, r *http.Request) {
+
+	var req AddNodeRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		RespondErr(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	res, err := router.nodeManager.AddNode(r.Context(), req.NodeAddress)
+	if err != nil {
+		RespondErr(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	RespondSuccess(w, http.StatusOK, &NodeInfoResponse{
+		NodeName:    res.NodeName,
+		XrayRunning: res.IsXrayRunning,
+	})
+}
